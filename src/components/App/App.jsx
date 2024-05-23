c1192fb0ae6015b719ce4687e781182bc59f09d0
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-23 23:16:18 +0300

    add contacts in contacts.json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,19 @@
 
-//import ContactForm from "../ContactForm/CntactForm.jsx"
-//import SearchBox from "../SearchBox/SearchBox.jsx"
-//import ContactList from "../ContactList/ContactList.jsx"
-//import contacts from "../../contacts.json"
+import ContactForm from "../ContactForm/CntactForm.jsx"
+import SearchBox from "../SearchBox/SearchBox.jsx"
+import ContactList from "../ContactList/ContactList.jsx"
+import contacts from "../../contacts.json"
 
-//export default function App(){
- //   return (
-  //      <div>
-  //         <h1>Phonebook</h1>
-  //         <ContactForm />
-  //         <SearchBox />
-   //        <ContactList />
-  //      </div>
+export default function App(){
+    return (
+        <div>
+           <h1>Phonebook</h1>
+           <ContactForm />
+           <SearchBox />
+           <ContactList />
+        </div>
 
- //   )
-//}
+    )
+}
 
 
