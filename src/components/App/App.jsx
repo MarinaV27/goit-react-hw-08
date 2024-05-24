6509ee3893922cff0ba23e717f92c7b17fa3106b
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-24 19:04:50 +0300

    add label in ContactForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,16 +4,20 @@ import SearchBox from "../SearchBox/SearchBox.jsx"
 import ContactList from "../ContactList/ContactList.jsx"
 import initialContacts from "../../contacts.json"
 import { useState } from "react"
-
+import css from "./App.module.css"
 
 export default function App(){
     const [contacts, setContacts] = useState ([initialContacts]);
+
+    const addContact = (newContact) => {}
+
+
     return (
-        <div>
+        <div className={css.container}>
            <h1>Phonebook</h1>
-           <ContactForm />
+           <ContactForm onAdd={addContact} />
            <SearchBox />
-           <ContactList />
+           <ContactList contacts={contacts} />
         </div>
 
     )
