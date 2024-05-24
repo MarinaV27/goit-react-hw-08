d225bb201a874e3b9ed105b89843a4e3c5d8e3e1
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-24 17:51:50 +0300

    add html in ContactList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,11 @@ import ContactForm from "../ContactForm/CntactForm.jsx"
 import SearchBox from "../SearchBox/SearchBox.jsx"
 import ContactList from "../ContactList/ContactList.jsx"
 import initialContacts from "../../contacts.json"
+import { useState } from "react"
+
 
 export default function App(){
+    const [contacts, setContacts] = useState ([initialContacts]);
     return (
         <div>
            <h1>Phonebook</h1>
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,16 @@
+
+import Contact from "../Contact/Contact.jsx"
+import css from "./ContactList.module.css"
+
+export default function ContactList ({contacts}) {
+    return (
+      <ul className={css.list}>
+      {contacts.map((contact) => (
+         <li className={css.item} key={contact.id}>
+            <Contact data={contact} />
+         </li>
+      ))}  
+      </ul> 
+
+    );
+}
