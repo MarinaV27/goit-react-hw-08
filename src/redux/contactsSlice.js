8cec7f8fd262d7c854ec7e24e49c75725f5250b8
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-17 23:23:22 +0200

    contactsSlice  all

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -7,8 +7,16 @@ const initialState = {
 const contactsSlice = createSlice({
     name: 'contacts',
     initialState,
-    reducers:{}
+    reducers: {
+        addContact: (state, action) => {
+            state.items.push(action.payload)
+        },
+        deleteContact: (state, action) => {
+            state.items.filter((item) => item.id !==action.payload)
+        }
+    }
 })
 
-
+export const { addContact, deleteContact } = contactsSlice.actions
 export default contactsSlice.reducer
+
