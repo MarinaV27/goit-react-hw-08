f87512e4a96c299e2aecb62f007cea6e9c169614
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-28 23:14:34 +0200

    add Route in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,9 @@ import { selectIsRefreshing } from '../../redux/auth/selectors.js';
 import { lazy, Suspense, useEffect } from "react"
 import {refreshUser} from '../../redux/auth/operations.js'
 import Layout from '../Layout/Layout.jsx'
+import LoginPage from '../../pages/LoginPage/LoginPage.jsx'
+import RegisterPage from '../../pages/RegisterPage/RegisterPage.jsx'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.jsx'
 //import { selectError, selectLoading } from "../../redux/contacts/selectors.js"
 
 const HomePage = lazy (() => import('../../pages/HomePage/HomePage.jsx'))
@@ -29,7 +32,10 @@ export default function App() {
         <Layout>
             <Suspense fallback={<div>Please wait..</div>}>
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/register' element={<RegisterPage />} />
+                        <Route path='*' element={<NotFoundPage />} />
                 </Routes>
             </Suspense>
         </Layout>
