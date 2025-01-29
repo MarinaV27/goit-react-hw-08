89d6b66d6a506f38771aa723059d398a0f831d83
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-29 21:59:27 +0200

    add </BrowserRouter> in main

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import 'modern-normalize'
 import App from './components/App/App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store} from './redux/store.js'
+import { store } from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
