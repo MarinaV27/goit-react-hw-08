22c67fd35f1d3106c512cb04ed5f998de20b0df0
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-29 21:29:01 +0200

    add LoginPage cod

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,5 @@
 
-//import ContactForm from "../ContactForm/ContactForm.jsx"
-//import SearchBox from "../SearchBox/SearchBox.jsx"
-//import ContactList from "../ContactList/ContactList.jsx"
-//import css from "./App.module.css"
+
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux"
 import { selectIsRefreshing } from '../../redux/auth/selectors.js';
@@ -12,7 +9,7 @@ import Layout from '../Layout/Layout.jsx'
 import LoginPage from '../../pages/LoginPage/LoginPage.jsx'
 import RegisterPage from '../../pages/RegisterPage/RegisterPage.jsx'
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.jsx'
-//import { selectError, selectLoading } from "../../redux/contacts/selectors.js"
+
 
 const HomePage = lazy (() => import('../../pages/HomePage/HomePage.jsx'))
 
@@ -23,8 +20,7 @@ export default function App() {
         dispatch(refreshUser());
     }, [dispatch]);
 
-    //  const error = useSelector(selectError)
-    //  const loading = useSelector(selectLoading)
+    
 
     return isRefreshing ? (
         <p>Refreshing user please wait...</p>
@@ -41,15 +37,4 @@ export default function App() {
         </Layout>
     )
 }
-        //<div className={css.container}>
-        //   <h1>Phonebook</h1>
-        //   <ContactForm />
-        //   <SearchBox />
-       //     <ContactList />
-       //     {error && <h2>Щось пішло не так</h2>}
-       //     {loading && <h2>Loading...</h2> }
-      //  </div>
-
-   // )
-
-
+       
