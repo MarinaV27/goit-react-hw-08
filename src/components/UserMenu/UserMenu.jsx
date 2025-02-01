75226be96833560cb7487708502e46eb2b4bb980
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-01 17:54:33 +0200

    add in auth/operations Bearer ${token}

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,16 +1,32 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import css from './UserMenu.module.css'
 import { selectUser } from '../../redux/auth/selectors'
+import { toast } from 'react-hot-toast'
+import { logoutThunk } from '../../redux/auth/operations';
 
 const UserMenu = () => {
-  
-  
-  const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const handleLogout = () => {
+    dispatch(logoutThunk())
+      .unwrap()
+      .then(() => {
+        toast.success('Good luck! See ya later.', {
+          icon: '👋',
+        });
+      })
+      .catch(error => {
+        toast.error('Logout failed. Please try again.');
+        console.log(error.message);
+      });
+  };
+
+
   
     return (
     <div className={css.wrapper}>
           <p className={css.text}>Welcome, { user.name } 🙂 </p>
-            <button className={css.btn} type='button' onClick={}>
+            <button className={css.btn} type='button' onClick={handleLogout}>
                 Logout
           </button>
     </div>
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -17,9 +17,24 @@ export const registerThunk = createAsyncThunk('auth/register', async (credential
 
 export const loginThunk = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
     try {
-        const { data } = await axios.post('auth/login', credentials)
+        const { data } = await axios.post('users/login', credentials)
         return data
     } catch (error) {
         return thunkAPI.rejectWithValue(error.message)
     }
 })
+
+export const logoutThunk = createAsyncThunk('auth/logout', async (__dirname, thunkAPI) => {
+    const token = thunkAPI.getState().auth.token
+     try {
+         const { data } = await axios.post('users/logout',
+             {}, {
+             headers: {
+                 Authorization: `Bearer ${token}`
+             }
+         });
+         return data;
+     } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+     }
+ })
