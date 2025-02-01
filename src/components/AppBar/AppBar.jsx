2a47d8fb56d662249b53962817c6d3f63fa51a39
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-01 16:55:02 +0200

    AppBar add navigation

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,17 @@
 import css from './AppBar.module.css'
 import Navigation from '../Navigation/Navigation'
+import { useSelector } from 'react-redux'
+import { selectIsLoggedIn } from '../../redux/auth/selectors'
+import UserMenu from '../UserMenu/UserMenu'
+import AuthNav from '../AuthNav/AuthNav'
 
 
 export default function AppBar() {
+    const isLoggedin = useSelector(selectIsLoggedIn);
     return (
         <div className={css.header}>
             <Navigation />
+            {isLoggedin ? <UserMenu /> : <AuthNav />}
         </div>
     )
 }
